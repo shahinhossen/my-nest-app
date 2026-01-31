6e50ac5b639df31939535691a281afd0fe54fae8
shahinhossen <shahin.rana.cs@gmail.com>
2026-01-31 22:27:38 +0600

    Check guard

---
+++ src/guard/auth/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = request.headers["authorization"];
+
+    return token === 'Bearer my-token';
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/guard/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   getAllUsers() {
     return this.userService.getAllUsers();
   }
